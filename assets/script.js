20f7b7fd85f62805f2e6b532f87dc2626f82d2bb
unknown <rinosaputra133@gmail.com>
2022-01-24 23:04:20 +0800

    covid-19

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+let getInput=document.querySelector(".search input");
+let getConfirm=document.querySelector(".data-result .total-case h2");
+let getRecovered=document.querySelector(".data-result .recover h2");
+let getDeath=document.querySelector(".data-result .death h2");
+let update=document.querySelector(".data-result .total-case");
+let getCountry=document.querySelector(".to-render h4");
+
+fetch("https://covid-19-data.p.rapidapi.com/help/countries", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		"x-rapidapi-key": "8e77efeeaamsh6718fd2ddf40f30p1fc783jsnf24246400435"
+	}
+})
+.then(response => response.json())
+.then(response=>{
+	let list=document.querySelector(".country");
+	let getName='';
+	response.forEach(obj=>{
+		getName+=`<h4>${obj.name}</h4>`;
+	});
+	list.innerHTML=`<h2>List Countries</h2>${getName}`;
+})
+
+getInput.addEventListener("keypress",function(e){
+	if(e.keyCode==13){
+		let countryName=e.target.value;
+		fetch(`https://covid-19-data.p.rapidapi.com/country?name=${countryName}`, {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+				"x-rapidapi-key": "8e77efeeaamsh6718fd2ddf40f30p1fc783jsnf24246400435"
+			}
+		})
+		.then(response => response.json())
+		.then(response=>{
+			console.log(response)
+			getConfirm.textContent=response[0].confirmed;
+			getRecovered.textContent=response[0].recovered;
+			getDeath.textContent=response[0].deaths;
+			getCountry.textContent=response[0].country;
+		});
+	}
+})
+
